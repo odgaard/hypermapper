2680a62034a07ecb569247232c96b98011e4b6a8
hvarfner <carl.hvarfner@live.se>
2024-04-23 08:42:28 +0200

    Added computationally viable alternative for Carl

--- hypermapper/bo/bt_acq.py
+++ hypermapper/bo/bt_acq.py
@@ -9,7 +9,9 @@ from botorch.acquisition.multi_objective.logei import(
     qLogNoisyExpectedHypervolumeImprovement
 )
 from botorch.acquisition.utils import prune_inferior_points
+from botorch.utils.multi_objective import get_chebyshev_scalarization
 from hypermapper.param.transformations import preprocess_parameters_array
+from botorch.acquisition.objective import ScalarizedPosteriorTransform
 
 
 class BotorchAcquisitionFunction: # well, basically only the qEI-family
@@ -48,8 +50,15 @@ class qLogNEI(BotorchAcquisitionFunction):
 
     def __init__(self, model):
         model = model
-        X_baseline = prune_inferior_points(model, model.train_inputs[0])
-        self.acq = self.acq_class(model, X_baseline)
+        X_baseline = model.train_inputs[0]
+        if X_baseline.ndim == 3:
+            X_baseline = X_baseline[0]
+        if model.num_outputs > 1:
+            weights = 0.1 + torch.rand(model.num_outputs).to(X_baseline)
+            posterior_transform = ScalarizedPosteriorTransform(weights=weights/weights.sum())
+        else:
+            posterior_transform = None
+        self.acq = self.acq_class(model, X_baseline, prune_baseline=True, posterior_transform=posterior_transform)
 
 
 class qLogNEHVI(BotorchAcquisitionFunction):
@@ -62,3 +71,5 @@ class qLogNEHVI(BotorchAcquisitionFunction):
         self.acq = self.acq_class(model, ref_point, model.train_inputs[0][0], prune_baseline=True)
 
 
+class qScalarizedNEI(BotorchAcquisitionFunction):
+    pass
