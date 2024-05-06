Random forest bugfix - double normalization of data

hypermapper/bo/models/rf.py
@@ -240,7 +240,6 @@ class RFClassificationModel(RandomForestClassifier):
         Returns:
             - tensor with model predictions.
         """
-        normalized_data, names = preprocess_parameters_array(data, self.param_space)
-        return torch.tensor(self.predict_proba(normalized_data.numpy()))[
+        return torch.tensor(self.predict_proba(data.numpy()))[
             :, list(self.classes_).index(True)
         ]

hypermapper/param/space.py
@@ -818,7 +818,8 @@ class Space:
         print(new_config_list)
         print(configurations)
 
-        new_conf = Tensor([[param[1] for param in config] for config in new_config_list])
+        new_conf = Tensor([[eval(param[1]) for param in config] for config in new_config_list])
+
         new_conf = new_conf.to(torch.float64)
         print(new_conf)
 